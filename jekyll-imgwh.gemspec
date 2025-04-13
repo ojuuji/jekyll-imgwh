78b6dbaf6e3e0cda1a13717721853cf91d12b481
Mikalai Ananenka <ojuuji@gmail.com>
2025-04-13 11:21:42 +0300

    initial implementation

---
+++ jekyll-imgwh.gemspec
@@ -0,0 +1,15 @@
+Gem::Specification.new do |spec|
+  spec.name        = "jekyll-imgwh"
+  spec.version     = "0.1"
+  spec.summary     = "Jekyll tag for HTML <img> element with auto-generated size attributes"
+  spec.description = "Jekyll tag for HTML <img> element which verifies image exists and automatically adds size attributes."
+  spec.authors     = ["Mikalai Ananenka"]
+  spec.email       = ["ojuuji@gmail.com"]
+  spec.files       = ["lib/jekyll-imgwh.rb"]
+  spec.homepage    = "https://github.com/ojuuji/jekyll-imgwh"
+  spec.license     = "MIT"
+  spec.required_ruby_version = ">= 3.0"
+
+  spec.add_dependency "jekyll", [">= 4.0", "< 5.0"]
+  spec.add_dependency "fastimage", [">= 2.4", "< 3.0"]
+end
---
+++ lib/jekyll-imgwh.rb
@@ -0,0 +1,46 @@
+require 'jekyll'
+require 'fastimage'
+
+class JekyllImgwhTag < Liquid::Tag
+  def initialize(tagName, content, tokens)
+    super
+    @content = content.strip
+  end
+
+  def render(context)
+    prefix = "jekyll-imgwh:"
+    Jekyll.logger.debug prefix, "---"
+    Jekyll.logger.debug prefix, "content: '#{@content}'"
+
+    if m = @content.match(/^(["'])((?:\1\1|(?!\1).)+)\1(.*)$/)
+      quote, src, rest = m.captures
+      src = src.sub("#{quote}#{quote}", quote)
+
+    elsif m = @content.match(/^((?:(?!\{\{)\S|\{\{.+?\}\})+)(.*)$/)
+      src, rest = m.captures
+
+    else
+      raise SyntaxError, "invalid img parameter: '#{@content}'"
+    end
+
+    Jekyll.logger.debug prefix, "src: '#{src}'"
+    Jekyll.logger.debug prefix, "rest: '#{rest}'"
+
+    src = Liquid::Template.parse(src).render(context)
+    Jekyll.logger.debug prefix, "src rendered: '#{src}'"
+
+    path = File.join(context.registers[:site].source, src).sub(/\?.*/, '')
+    Jekyll.logger.debug prefix, "image path: '#{path}'"
+    File.file?(path) or raise LoadError, "image file '#{path}' could not be found"
+
+    size = FastImage.size(path) or raise LoadError, "could not get size of image file '#{path}'"
+    Jekyll.logger.debug prefix, "image size: #{size}"
+
+    rest = Liquid::Template.parse(rest).render(context)
+    Jekyll.logger.debug prefix, "rest rendered: '#{rest}'"
+
+    "<img src=\"#{src}\" width=\"#{size[0]}\" height=\"#{size[1]}\"#{rest}>"
+  end
+
+  Liquid::Template.register_tag "img", self
+end
