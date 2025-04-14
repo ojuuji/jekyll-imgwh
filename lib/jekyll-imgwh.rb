eb9b24cf629c4f0d288826d8938d9a0d383b7034
Mikalai Ananenka <ojuuji@gmail.com>
2025-04-15 02:17:41 +0300

    URL-decode image path

--- lib/jekyll-imgwh.rb
+++ lib/jekyll-imgwh.rb
@@ -1,6 +1,7 @@
+require 'cgi'
 require 'fastimage'
 require 'jekyll'
-require 'jekyll-img/version'
+require 'jekyll-imgwh/version'
 
 module Jekyll::Imgwh
   class Tag < Liquid::Tag
@@ -36,7 +37,7 @@ module Jekyll::Imgwh
       src = Liquid::Template.parse(src).render(context)
       debug "src rendered: '#{src}'"
 
-      relative_path = src.sub(/\?.*/, '')
+      relative_path = CGI.unescape(src.sub(/\?.*/, ''))
       path = File.join(context.registers[:site].source, relative_path)
       debug "image path: '#{path}'"
 
--- lib/jekyll-imgwh/version.rb
+++ lib/jekyll-imgwh/version.rb
@@ -1,6 +1,6 @@
 module Jekyll
   module Imgwh
     NAME = "jekyll-imgwh"
-    VERSION = "0.4.1"
+    VERSION = "0.4.2"
   end
 end
