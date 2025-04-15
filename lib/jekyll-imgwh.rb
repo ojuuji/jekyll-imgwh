0842afe148e7892c314682bc0502e947283cd99e
Mikalai Ananenka <ojuuji@gmail.com>
2025-04-15 03:24:23 +0300

    add support for relative paths

--- lib/jekyll-imgwh.rb
+++ lib/jekyll-imgwh.rb
@@ -37,19 +37,7 @@ module Jekyll::Imgwh
       src = Liquid::Template.parse(src).render(context)
       debug "src rendered: '#{src}'"
 
-      relative_path = CGI.unescape(src.sub(/\?.*/, ''))
-      path = File.join(context.registers[:site].source, relative_path)
-      debug "image path: '#{path}'"
-
-      unless File.file?(path)
-        theme_root = context.registers[:site].config.dig(NAME, "theme_root") or raise LoadError, "#{NAME}: image '#{path}' could not be found"
-        themed_path = File.join(theme_root, relative_path)
-        debug "themed image path: '#{themed_path}'"
-
-        File.file?(themed_path) or raise LoadError, "#{NAME}: none of images '#{path}', '#{themed_path}' could be found"
-        path = themed_path
-      end
-
+      path = resolve_path src, context
       size = FastImage.size(path) or raise LoadError, "#{NAME}: could not get size of image '#{path}'"
       debug "image size: #{size}"
 
@@ -59,6 +47,29 @@ module Jekyll::Imgwh
       "<img src=#{quote}#{src}#{quote} width=#{quote}#{size[0]}#{quote} height=#{quote}#{size[1]}#{quote}#{rest.empty? ? "" : " #{rest}"}>"
     end
 
-    Liquid::Template.register_tag Jekyll::configuration().dig(NAME, "tag_name") || "img", self
+    def resolve_path(src, context)
+      try_themed = true
+      relative_path = CGI.unescape(src.sub(/\?.*/, ''))
+
+      unless relative_path.start_with?('/')
+        try_themed = false
+        relative_path = File.join(context.registers[:page]['dir'], relative_path)
+      end
+
+      path = File.join(context.registers[:site].source, relative_path)
+      debug "image path: '#{path}'"
+
+      return path if File.file?(path)
+
+      theme_root = context.registers[:site].config.dig(NAME, 'theme_root') or raise LoadError, "#{NAME}: image '#{path}' could not be found"
+      themed_path = File.join(theme_root, relative_path)
+      debug "themed image path: '#{themed_path}'"
+
+      return themed_path if File.file?(themed_path)
+
+      raise LoadError, "#{NAME}: none of images '#{path}', '#{themed_path}' could be found"
+    end
+
+    Liquid::Template.register_tag Jekyll::configuration().dig(NAME, 'tag_name') || 'img', self
   end
 end
--- lib/jekyll-imgwh/version.rb
+++ lib/jekyll-imgwh/version.rb
@@ -1,6 +1,6 @@
 module Jekyll
   module Imgwh
     NAME = "jekyll-imgwh"
-    VERSION = "0.5.1"
+    VERSION = "0.5.2"
   end
 end
